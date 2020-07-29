b89a0a01318f50925b0f78efd10cb295ee8f6732
Oluwatomisin <tomisinl@ebanqo.com>
2020-07-29 15:23:25 +0100

    MyName Component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <MyName name="Oluwatomisin Lalude" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyName from "./components/MyName.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MyName,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1 class="text-center align-middle">{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyName",
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  position: relative;
+  top: 45vh;
+}
+</style>
