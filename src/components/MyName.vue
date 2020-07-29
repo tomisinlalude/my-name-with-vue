5e1cd24571e1ba1798af81ccde270162a427963c
Oluwatomisin <tomisinl@ebanqo.com>
2020-07-29 15:46:24 +0100

    Adjust text position

--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="mt-5">
     <h1 class="text-center align-middle">{{ name }}</h1>
   </div>
 </template>
@@ -17,7 +17,5 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
-  position: relative;
-  top: 45vh;
 }
 </style>
